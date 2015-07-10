680a356a2eb8cb16bb82f90015ddcf29fa0fa4e1
Xavier Barbosa <clint.northwood@gmail.com>
2015-07-10 22:08:47 +0200

    implements derivated transit

--- aiovault/v1/secret/backends/transit.py
+++ aiovault/v1/secret/backends/transit.py
@@ -1,5 +1,5 @@
 from .bases import SecretBackend
-from aiovault.exceptions import InvalidPath
+from aiovault.exceptions import InvalidPath, InvalidRequest
 from aiovault.objects import Value
 from aiovault.util import base64_encode, base64_decode, ok, task
 
@@ -28,20 +28,46 @@ class TransitBackend(SecretBackend):
             raise KeyError('%r does not exists' % name)
 
     @task
-    def write_key(self, name):
+    def read_raw(self, name):
+        """Fetch raw keys for named encryption keys.
+
+        This path is used to get the underlying encryption keys used
+        for the named keys that are available
+
+        Parameters:
+            name (str): The transit key
+        Returns:
+            Value
+        """
+        method = 'GET'
+        path = self.path('raw', name)
+
+        try:
+            response = yield from self.req_handler(method, path)
+            result = yield from response.json()
+            print(result)
+            return Value(**result)
+        except InvalidPath:
+            raise KeyError('%r does not exists' % name)
+
+    @task
+    def write_key(self, name, *, derived=False):
         """Creates a new named encryption key.
 
         This is a root protected endpoint.
 
         Parameters:
             name (str): The transit key
+            derived (bool): Enables key derivation mode. This allows
+                            for per-transaction unique keys
         Returns:
             bool
         """
         method = 'POST'
+        data = {'derived': derived}
         path = self.path('keys', name)
 
-        response = yield from self.req_handler(method, path)
+        response = yield from self.req_handler(method, path, json=data)
         return ok(response)
 
     @task
@@ -63,40 +89,52 @@ class TransitBackend(SecretBackend):
         return ok(response)
 
     @task
-    def encrypt(self, key, plaintext):
+    def encrypt(self, key, plaintext, context=None):
         """Encrypts the provided plaintext using the named key.
 
         Parameters:
             key (str): The transit key
             plaintext (str): The plaintext to encrypt
+            context (str): Context for key derivation. Required for
+                           derived keys.
         Returns:
             Value
         """
         method = 'POST'
         path = self.path('encrypt', key)
-        data = {'plaintext': base64_encode(plaintext)}
+        data = {'plaintext': base64_encode(plaintext),
+                'context': base64_encode(context) if context else None}
 
-        response = yield from self.req_handler(method, path, json=data)
-        result = yield from response.json()
-        return Value(**result)
+        try:
+            response = yield from self.req_handler(method, path, json=data)
+            result = yield from response.json()
+            return Value(**result)
+        except InvalidRequest as error:
+            raise ValueError(error.errors.pop())
 
     @task
-    def decrypt(self, key, ciphertext):
+    def decrypt(self, key, ciphertext, context=None):
         """Decrypts the provided ciphertext using the named key.
 
         Parameters:
             key (str): The transit key
             ciphertext (str): The ciphertext to decrypt,
                               provided as returned by encrypt.
+            context (bool): Context for key derivation. Required for
+                            derived keys.
         Returns:
             Value
         """
         method = 'POST'
         path = self.path('decrypt', key)
-        data = {'ciphertext': ciphertext}
+        data = {'ciphertext': ciphertext,
+                'context': base64_encode(context) if context else None}
 
-        response = yield from self.req_handler(method, path, json=data)
-        result = yield from response.json()
+        try:
+            response = yield from self.req_handler(method, path, json=data)
+            result = yield from response.json()
+        except InvalidRequest as error:
+            raise ValueError(error.errors.pop())
         result = Value(**result)
         result['plaintext'] = base64_decode(result['plaintext'])
         return result
--- tests/test_transit.py
+++ tests/test_transit.py
@@ -3,6 +3,7 @@ from conftest import async_test
 import pytest
 
 PLAIN_TEXT = "the quick brown fox"
+CONTEXT = 'my-cool-context'
 
 
 @async_test
@@ -11,11 +12,17 @@ def test_basic(dev_server):
     mounted, backend = yield from client.secret.mount('transit')
     assert mounted
 
-    written = yield from backend.write_key('test')
+    written = yield from backend.write_key('test', derived=False)
     assert written
 
     policy = yield from backend.read_key('test')
     assert policy['name'] == 'test'
+    assert policy['derived'] is False
+
+
+    policy = yield from backend.read_raw('test')
+    assert policy['name'] == 'test'
+    assert policy['derived'] is False
 
     encrypted = yield from backend.encrypt('test', PLAIN_TEXT)
     assert 'ciphertext' in encrypted
@@ -30,6 +37,73 @@ def test_basic(dev_server):
 
     with pytest.raises(KeyError):
         yield from backend.read_key('test')
+
+    with pytest.raises(KeyError):
+        yield from backend.read_raw('test')
+
+
+@async_test
+def test_upsert(dev_server):
+    client = Vault(dev_server.addr, token=dev_server.root_token)
+    mounted, backend = yield from client.secret.mount('transit')
+    assert mounted
+
+    with pytest.raises(KeyError):
+        yield from backend.read_key('test')
+
+    encrypted = yield from backend.encrypt('test', PLAIN_TEXT)
+    assert 'ciphertext' in encrypted
+    ciphertext = encrypted['ciphertext']
+
+    policy = yield from backend.read_key('test')
+    assert policy['name'] == 'test'
+    assert policy['derived'] is False
+
+    decrypted = yield from backend.decrypt('test', ciphertext)
+    assert 'plaintext' in decrypted
+    assert decrypted['plaintext'] == PLAIN_TEXT
+
+    deleted = yield from backend.delete_key('test')
+    assert deleted
+
+    with pytest.raises(KeyError):
+        yield from backend.read_key('test')
+
+
+@async_test
+def test_derived(dev_server):
+    client = Vault(dev_server.addr, token=dev_server.root_token)
+    mounted, backend = yield from client.secret.mount('transit')
+    assert mounted
+
+    written = yield from backend.write_key('test', derived=True)
+    assert written
+
+    policy = yield from backend.read_key('test')
+    assert policy['name'] == 'test'
+    assert policy['derived'] is True
+
+
+    policy = yield from backend.read_raw('test')
+    assert policy['name'] == 'test'
+    assert policy['derived'] is True
+
+    encrypted = yield from backend.encrypt('test', PLAIN_TEXT, context=CONTEXT)
+    assert 'ciphertext' in encrypted
+    ciphertext = encrypted['ciphertext']
+
+    decrypted = yield from backend.decrypt('test', ciphertext, context=CONTEXT)
+    assert 'plaintext' in decrypted
+    assert decrypted['plaintext'] == PLAIN_TEXT
+
+    deleted = yield from backend.delete_key('test')
+    assert deleted
+
+    with pytest.raises(KeyError):
+        yield from backend.read_key('test')
+
+    with pytest.raises(KeyError):
+        yield from backend.read_raw('test')
 
 
 @async_test
